adc4f973dc5c489df8dd986d827129ac3b326017
Mikkel_Ard <ragnarlothbrok1126@gmail.com>
2022-07-13 20:58:49 -0600

    Creation of income and egress arrangements

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const income = [
+    new Income('Salario', 2100.00),
+    new Income('Venta conche', 1500)
+];
+
+const egress = [
+    new Egress('Renta departamento', 900),
+    new Egress('Ropa', 400)
+];
+
+let loadApp = () => {
+    loadheadboard();
+}
+
+let loadheadboard = () => {
+    
+}
